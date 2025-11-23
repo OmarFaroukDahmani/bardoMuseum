6068527ef1ac1c52791fff22c6b61e1b6d1fa47a
Omar Farouk Dahmani <dahmani03112005@gmail.com>
2025-11-23 16:04:04 +0100

    feat: finalize project implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-// Merged all 'react-router-dom' imports into one line
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 
-// Page Imports
 import Home from "./pages/Home"
 import Gallery from "./pages/Gallery"
 import Museum from "./pages/Museum"
 import Visit from "./pages/Visit"
 
-// Component Imports
 import Navbar from './components/Navbar';
 import Cursor from './components/Cursor';
 
@@ -19,11 +16,9 @@ function App() {
 
   return (
     <>
-      {/* These components are persistent and stay on every page */}
       <Cursor />
       <Navbar />
 
-      {/* AnimatePresence handles the exit/enter animations */}
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
           <Route index element={<Home />} />
